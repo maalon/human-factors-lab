b66be5b969bde43f65d19d4e7930d2761de5720e
maalon <91635450+maalon@users.noreply.github.com>
2023-01-26 21:22:42 +0200

    Update install_ws.sh

--- install_ws.sh
+++ install_ws.sh
@@ -14,14 +14,8 @@ git clone https://github.com/turtlebot/turtlebot_interactions.git
 cd ~/catkin_ws
 catkin_make 
 
-#adding the humans-factors-lab
-cd catkin_ws/src
-catkin_create_pkdg human-factors-lab rospy
-
-#moving the pilot script to the new repo
-mv turtlebot2/pilot.py ~/.catkin_ws/src/humans-factors-lab/
-
 # Copi install.sh in the new repo
+cd
 mkdir turtlebot2_new
 mv turtlebot2/install.sh turtlebot2_new/
 
