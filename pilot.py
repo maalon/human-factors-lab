bfe6619cd236646ea4fdac045854d5820ed30a76
maalon <91635450+maalon@users.noreply.github.com>
2023-02-02 12:14:02 +0200

    Update pilot.py

--- pilot.py
+++ pilot.py
@@ -8,18 +8,20 @@ import serial
 
 # Global variable to keep track of the number of times the experience is done
 experience_count = 0
-
-ser = serial.Serial('COM3', 9600) # specify the correct serial port and baud rate for your Arduino
+count = 0
+# ser = serial.Serial('COM3', 9600) # specify the correct serial port and baud rate for your Arduino
 
 # Create a list to store the distance values
 distances = []
-
+x = []
 def follower_callback(data):
     global experience_count
     arduino_data = ser.readline().decode().strip() # read the data from the Arduino
     # extract the necessary information from the odometry message
     x = data.pose.pose.position.x
-    y = data.pose.pose.position.y
+    # add the x of the robot
+    x = x.append(x)
+    # y = data.pose.pose.position.y
     # extract the distance from the person
     distance = data.pose.pose.position.z
     # add the distance to the list
@@ -33,10 +35,10 @@ def follower_callback(data):
     # get the current time
     current_time = time.strftime("%H:%M:%S", time.gmtime())
     # create the file name
-    file_name = 'pilot_data_{}_{}.csv'.format(current_date, experience_count)
+    file_name = 'pilot_data_{}_{}_{}.csv'.format(current_date,current_time, experience_count)
     # write the data to a CSV file
     with open(file_name, mode='a') as csv_file:
-        fieldnames = ['Time', 'X', 'Y', 'Distance', 'Distance Diff', 'Arduino Data']
+        fieldnames = ['Time', 'X', 'Distance', 'Distance Diff', 'r-speed', 'h-speed']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writerow({'Time': current_date, 'X': x, 'Y': y, 'Distance': distance, 'Distance Diff': distance_diff, 'Arduino Data': arduino_data})
+        writer.writerow({'Time': current_time, 'X': x, 'Distance': distance, 'Distance Diff': distance_diff, 'r-speed': ,'h-speed': })
     print("Data written to CSV file")
